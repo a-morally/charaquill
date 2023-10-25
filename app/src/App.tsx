034aeeb7a970a47e6178d76d5825af45742553b8
a-morally <apathydriven@gmail.com>
2023-10-26 01:06:46 +0300

    basic design and prettier

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,19 @@
+import { Card } from "./components/Card";
+
 function App() {
   return (
-    <div id="app" class="w-screen h-screen bg-primary-900">
-      <div class="text-primary-100">
-        Hello there!
-        <br /> It's a nice start of a long journey :)
+    <div id="app" class="bg-primary-900 h-screen w-screen pt-12">
+      <div class="bg-primary-800 fixed top-0 flex h-12 w-full justify-between self-center p-2 shadow-md">
+        <button class="text-primary-100">Menu</button>
+        <button class="text-primary-100">Settings</button>
+      </div>
+
+      <div class="grid grid-cols-1 gap-2 p-2 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:max-w-screen-lg lg:mx-auto">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
       </div>
     </div>
   );
